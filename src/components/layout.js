2b494c379377398c7d117ff02000c53b1dcbb337
Jason <jasonakie28@gmail.com>
2021-01-02 15:35:07 +1100

    add react-reveal lib

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,9 @@
 import React from "react"
 import { Link } from "gatsby"
 import "./layout.css"
+import "./styles.css"
 import { AnchorLink } from "gatsby-plugin-anchor-links";
+import { slide as Menu } from 'react-burger-menu'
 
 const CustomLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem`}}>
@@ -12,17 +14,25 @@ const CustomLink = props => (
 export default function Layout({ children }) {
   return (
     <div>
-        <header style={{marginBottom: `1rem`,height: `3rem`, position: "sticky", top: 0, zIndex: 100, backgroundColor:"black"}}>
+       
+        <header style={{marginBottom: `1rem`,height: `3.5rem`, position: "sticky", top: 0, zIndex: 100, backgroundColor:"black"}}>
+        {/* <Menu right> */}
+               
+            {/* </Menu> */}
             <AnchorLink offset="-100" to="/#top" style={{ textShadow: `none`, backgroundImage: `none` }}>
-              <h3 style={{ display: "inline",lineHeight: `3rem`, marginLeft: `1rem`, color:"white"}}>Jason<i>kie</i></h3>
+              <h3 style={{ display: "inline",lineHeight: `3.5rem`, marginLeft: `1rem`, color:"white"}}>Jason<i>kie</i></h3>
             </AnchorLink>
+           
             <ul style={{listStyle: `none`, float: `right`, overflow:"hidden", lineHeight: `3rem`}}>
-              <CustomLink to="/#work" >Works</CustomLink>
-              <CustomLink to="/#skills" >Skills</CustomLink>
-              <CustomLink to="/#education">Education</CustomLink>
-              <CustomLink to="/contact/">Contact</CustomLink>
+            <CustomLink className="menu-item" to="/#work" >Works</CustomLink>
+               <CustomLink className="menu-item" to="/#skills" >Skills</CustomLink>
+               <CustomLink className="menu-item" to="/#education">Education</CustomLink>
+               <CustomLink className="menu-item" to="/contact/">Contact</CustomLink>
             </ul>
+            
+            
         </header>
+      
         <div style={{ margin: `0rem auto`, maxWidth: 900, padding: `0rem 2rem` }}>
          {children}
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import {
   BarChart, Bar, XAxis, YAxis, LabelList, ResponsiveContainer
 } from 'recharts';
+import Fade from 'react-reveal/Fade';
 
 const data = [
   {
@@ -28,28 +29,43 @@ const data = [
 export default function Home() {
   return (
     <Layout>
+      <Fade left>
       <h1 id="top">Hi! I’m Jason Kie and I’m a Technical Business Analyst</h1>
+      </Fade>
       <br>
       </br>
-      <p>
-      I identify and streamline functional operations in a technical business project by promoting system efficiency
-       with the aid of data analysis.
-      </p>
+      <Fade right>
+      <p> I identify and streamline functional operations in a technical business project by promoting system efficiency
+       with the aid of data analysis.</p>
+      </Fade>
+      <Fade left>
       <h3 id="work">Work Experience</h3>
+      </Fade>
+      <Fade left>
       <li>Technical Business Analyst - Alex Solutions</li>
+      </Fade>
+      <Fade right>
       <p>
       My role involves assessing web applications for performance bottlenecks and improving the 
       performance testing process by creating an automation pipeline. Moreover, I manage AWS Instances for testing. 
       I visualise and report the performance metrics to stakeholders for easy analysis.
       </p>
+      </Fade>
+      <Fade left>
       <h4>Reporting Analyst - Origin Energy</h4>
+      </Fade>
+      <Fade right>
       <ul>
         <li>Ensure the timeliness and accuracy of information to the business to support business processes, monitor performance and enable decision making
         </li>
         <li>Develop business reports which mainly use Tableau, maintain the reports to eliminate any data discrepancies present</li>
         <li>Work with data to provide meaning and value for the business including data transformation, data visualization and analytics</li>
       </ul>
+      </Fade>
+      <Fade left>
       <h3 id="skills">Skills</h3>
+      </Fade>
+      <Fade right>
       <ResponsiveContainer width="100%" height={400}>
         <BarChart
           layout="vertical"
@@ -68,10 +84,19 @@ export default function Home() {
           </Bar>
         </BarChart>
       </ResponsiveContainer>
+      </Fade>
+      <Fade left>
       <h3 id="education">Education</h3>
+      </Fade>
+      <Fade right>
       <p>Bachelor of Information Technology (Software Development)</p>
+      </Fade>
+      <Fade left>
       <h3>Awards</h3>
+      </Fade>
+      <Fade right>
       <p>Monash University Industry Based Learning Scholarship</p>
+      </Fade>
       <h3 id="contact">Contact me</h3>
     </Layout>
   );
